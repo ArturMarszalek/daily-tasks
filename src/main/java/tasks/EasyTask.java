0175758955af19f3439f135497cc6445ae750511
Rafał Załęski <rafalzaleski@gmail.com>
2020-02-15 09:54:27 +0100

    push

--- src/main/java/tasks/EasyTask.java
+++ src/main/java/tasks/EasyTask.java
@@ -10,4 +10,3 @@ public class EasyTask extends TaskBase{
         return 5;
     }
 }
-//commentt
--- src/main/java/tasks/list/MyLinkedList.java
+++ src/main/java/tasks/list/MyLinkedList.java
@@ -2,49 +2,49 @@ package tasks.list;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+
 //TO START -> UNCOMMENT THIS
-//public class MyLinkedList<T> implements Iterable<T> {
-//AND -> COMMENT THIS
-public class MyLinkedList<T> extends LinkedList<T> {
+public class MyLinkedList<T> implements Iterable<T> {
+    //AND -> COMMENT THIS
+//public class MyLinkedList<T> extends LinkedList<T> {
     private MyLinkedListElement<T> first;
     private MyLinkedListElement<T> last;
 
-//UNCOMMENT BELOW
-//    public boolean add(T task) {
-//        return false;
-//    }
-//
-//    public void add(int index, T taskBase) {
-//
-//    }
-//
-//    public boolean remove(T task) {
-//        return false;
-//    }
-//
-//    public boolean contains(T task) {
-//        return false;
-//    }
-//
-//    public T get(int index) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Iterator<T> iterator() {
-//       return new Iterator<T>() {
-//             //SOMETHING TO ADD HERE?
-//           @Override
-//           public boolean hasNext() {
-//               //TODO CHANGE THIS
-//               return false;
-//           }
-//
-//           @Override
-//           public T next() {
-//               //TODO CHANGE THIS
-//               return null;
-//           }
-//       };
-//    }
+    public boolean add(T task) {
+        return false;
+    }
+
+    public void add(int index, T taskBase) {
+
+    }
+
+    public boolean remove(T task) {
+        return false;
+    }
+
+    public boolean contains(T task) {
+        return false;
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            //SOMETHING TO ADD HERE?
+            @Override
+            public boolean hasNext() {
+                //TODO CHANGE THIS
+                return false;
+            }
+
+            @Override
+            public T next() {
+                //TODO CHANGE THIS
+                return null;
+            }
+        };
+    }
 }
