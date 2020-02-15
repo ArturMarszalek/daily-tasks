db7efa184788a414e1565b47abce888169048076
Szorstki <baiokai@gmail.com>
2020-02-15 13:55:42 +0100

    Refactor Remove and Contains methods

--- src/main/java/tasks/list/MyLinkedList.java
+++ src/main/java/tasks/list/MyLinkedList.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 public class MyLinkedList<T> implements Iterable<T> {
 
     //public class MyLinkedList<T> extends LinkedList<T> {
-//
+
     private MyLinkedListElement<T> first;
     private MyLinkedListElement<T> last;
 
@@ -28,23 +28,45 @@ public class MyLinkedList<T> implements Iterable<T> {
     }
 
     public boolean remove(T task) {
+        MyLinkedListElement<T> currentElement = first;
+        MyLinkedListElement<T> previousElement = null;
+        do {
+            if (task == currentElement.getElement()) {
+                if (currentElement == first) {
+                    first = currentElement.getNext();
+                    return true;
+                }
+                if (currentElement == last) {
+                    last = previousElement;
+                    previousElement.setNext(null);
+                    return true;
+                }
+                previousElement.setNext(currentElement.getNext());
+                return true;
+            }
+            previousElement = currentElement;
+            currentElement = currentElement.getNext();
+        } while (currentElement != null);
         return false;
     }
 
+
     public boolean contains(T task) {
-        MyLinkedListElement<T> elemntOnList = first;
-        if (first != null) {
-            do {
-                if (task == elemntOnList.getElement()) {
-                    return true;
-                } else {
-                    elemntOnList = elemntOnList.getNext();
-                }
-            } while (elemntOnList.getNext() != null);
+        if (first == null) {
             return false;
         }
+        MyLinkedListElement<T> elemntOnList = first;
+
+        do {
+            if (task == elemntOnList.getElement()) {
+                return true;
+            } else {
+                elemntOnList = elemntOnList.getNext();
+            }
+        } while (elemntOnList != null);
         return false;
     }
+
 
     public T get(int index) {
         return null;
@@ -54,6 +76,7 @@ public class MyLinkedList<T> implements Iterable<T> {
     public Iterator<T> iterator() {
         return new Iterator<T>() {
             MyLinkedListElement<T> currentElement = first;
+
             @Override
             public boolean hasNext() {
                 return currentElement != null;
@@ -61,7 +84,7 @@ public class MyLinkedList<T> implements Iterable<T> {
 
             @Override
             public T next() {
-                if(hasNext()){
+                if (hasNext()) {
                     T element = currentElement.getElement();
                     currentElement = currentElement.getNext();
                     return element;
