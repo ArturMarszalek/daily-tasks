e11bcb0cc84f02f324d513390e39476a5994ba19
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-02-15 12:30:29 +0100

    tests: add & shouldCountAllPointsForFinishedTasks()

--- src/main/java/tasks/list/MyLinkedList.java
+++ src/main/java/tasks/list/MyLinkedList.java
@@ -2,15 +2,27 @@ package tasks.list;
 
 import java.util.Iterator;
 import java.util.LinkedList;
-//TO START -> UNCOMMENT THIS
+
 public class MyLinkedList<T> implements Iterable<T> {
-//AND -> COMMENT THIS
-//public class MyLinkedList<T> extends LinkedList<T> {
+
     private MyLinkedListElement<T> first;
     private MyLinkedListElement<T> last;
+    private MyLinkedListElement<T> current=null;
 
-//UNCOMMENT BELOW
+    boolean initialize = true;
+
     public boolean add(T task) {
+        MyLinkedListElement<T> newTask = new MyLinkedListElement<>(task);
+        if (first == null) {
+            first = newTask;
+            last = newTask;
+            return true;
+        } else if (first != null) {
+            last.setNext(newTask);
+            last = newTask;
+
+        }
+
         return false;
     }
 
@@ -19,10 +31,30 @@ public class MyLinkedList<T> implements Iterable<T> {
     }
 
     public boolean remove(T task) {
+//        MyLinkedListElement tmp=new MyLinkedListElement(first);
+//    while (tmp.hasNext()){
+//        if (tmp==task){
+//
+//        }
+//        tmp=tmp.getNext();
+////    }
+//        return false;
         return false;
     }
 
     public boolean contains(T task) {
+        if (first==null){
+            return false;
+        }
+
+        MyLinkedListElement<T> elemntOnList = first;
+        do {
+            if (task == elemntOnList.getElement()) {
+                return true;
+            } else {
+                elemntOnList = elemntOnList.getNext();
+            }
+        } while (elemntOnList.getNext() != null);
         return false;
     }
 
@@ -32,19 +64,20 @@ public class MyLinkedList<T> implements Iterable<T> {
 
     @Override
     public Iterator<T> iterator() {
-       return new Iterator<T>() {
-             //SOMETHING TO ADD HERE?
-           @Override
-           public boolean hasNext() {
-               //TODO CHANGE THIS
-               return false;
-           }
+        return new Iterator<T>() {
+            MyLinkedListElement<T> nextElement = first;
+            @Override
+            public boolean hasNext() {
+                if (nextElement==null){return false;}
+                return nextElement != last.getNext();
+            }
+            @Override
+            public T next() {
+                T element = nextElement.getElement();
+                nextElement = nextElement.getNext();
+                return element;
 
-           @Override
-           public T next() {
-               //TODO CHANGE THIS
-               return null;
-           }
-       };
+            }
+        };
     }
 }
