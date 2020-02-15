2cae6c22e26ab0187fe4d8c10144339a61268ca1
Rafał Załęski <rafalzaleski@gmail.com>
2020-02-15 11:53:36 +0100

    push

--- src/main/java/tasks/list/MyLinkedList.java
+++ src/main/java/tasks/list/MyLinkedList.java
@@ -9,8 +9,18 @@ public class MyLinkedList<T> implements Iterable<T> {
 //public class MyLinkedList<T> extends LinkedList<T> {
     private MyLinkedListElement<T> first;
     private MyLinkedListElement<T> last;
+    private MyLinkedListElement<T> next;
+
 
     public boolean add(T task) {
+        MyLinkedListElement<T> myNewElement = new MyLinkedListElement<>(task);
+        if (first == null) {
+            first = myNewElement;
+            last = myNewElement;
+        } else {
+            last.setNext(myNewElement);
+            last = myNewElement;
+        }
         return false;
     }
 
@@ -23,6 +33,17 @@ public class MyLinkedList<T> implements Iterable<T> {
     }
 
     public boolean contains(T task) {
+        if (first == null) {
+            return false;
+        }
+        MyLinkedListElement<T> currentElement = first;
+        do {
+            if (task == currentElement.getElement()) {
+                return true;
+            } else {
+                currentElement = currentElement.getNext();
+            }
+        } while (currentElement != null);
         return false;
     }
 
--- src/main/java/tasks/list/MyLinkedListElement.java
+++ src/main/java/tasks/list/MyLinkedListElement.java
@@ -2,12 +2,24 @@ package tasks.list;
 
 public class MyLinkedListElement<T> {
     private T element;
+    private MyLinkedListElement<T> next;
 
     public MyLinkedListElement(T element) {
         this.element = element;
     }
 
+    public MyLinkedListElement<T> getNext() {
+        return next;
+    }
+
+    public void setNext(MyLinkedListElement<T> next) {
+        this.next = next;
+    }
+
     public T getElement() {
         return element;
     }
+
+
+
 }
